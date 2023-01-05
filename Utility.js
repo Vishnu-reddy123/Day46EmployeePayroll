17f2e42d4b474013d96aa2e6df30f837f7d00c94
Vishnu-reddy123 <vishnu95532@gmail.com>
2023-01-05 10:58:56 +0530

    added logic for saving updated  employee payroll details to local storage

--- Utility.js
+++ Utility.js
@@ -1,11 +1,5 @@
 const stringifyDate=(date) => {
     const options={day: 'numeric', month:'short', year:'numeric'};
-    const newDate=!date ? "undefined": new Date(Date.parse(date)).toLocaleDateString('en-G8',options);
+    const newDate=!date ? "undefined": new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
     return newDate;
-}
-const update = (node) => {
-    let empPayrollData = empPayrollList.find(empData => empData._name == node.id);
-    if(!empPayrollData) return;
-    localStorage.setItem('editEmp', JSON.stringify(empPayrollData));
-    window.location.replace(siteProperties.add_emp_payroll_page);
 }
